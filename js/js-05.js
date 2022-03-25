e7af640cc54a74d5cc2b8fb45ba9dbeacdaf34be
Aleksey Smychenko <93550750+alfrntlower@users.noreply.github.com>
2022-03-25 02:04:38 +0200

    HW 5
    Tasks 8 to 10

--- js/js-05.js
+++ js/js-05.js
@@ -227,19 +227,97 @@ price - цена автомобиля.
 
 /*
 Автопроверки 5.8
+Выполни рефакторинг класса Car так, чтобы он принимал один параметр - объект со свойсвами brand, model и price. 
+Деструктуризируй объект в сигнатуре (подписи) конструктора.
 */
+
+// class Car {
+//   // Change code below this line
+//   constructor({brand, model, price}) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
+
+// console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 })) // { brand: "Audi", model: "Q3", price: 36000 }
+// console.log(new Car({ brand: "BMW", model: "X5", price: 58900 })) // { brand: "BMW", model: "X5", price: 58900 }
+// console.log(new Car({ brand: "Nissan", model: "Murano", price: 31700 })) // { brand: "Nissan", model: "Murano", price: 31700 }
 
 
 
 /*
 Автопроверки 5.9
+Добавь классу Car два метода.
+
+getPrice() - возвращает значение свойства price из объекта который его будет вызывать.
+changePrice(newPrice) - обновляет значение свойства price у объекта который его будет вызывать на newPrice.
 */
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+
+//     getPrice() {
+
+//         return this.price;
+//     }
+
+//     changePrice(newPrice) {
+//         return this.price = newPrice;
+//     }
+
+//   // Change code above this line
+// }
 
 
 
 /*
 Автопроверки 5.10
+Напиши класс Storage, который будет создавать объекты для управления складом товаров. 
+Класс ожидает только один аргумент - начальный массив товаров, который записывается на создаваемый объект в свойство items.
+
+Объяви следующие методы класса:
+
+getItems() - возвращает массив текущих товаров в свойстве items объекта который вызывает этот метод.
+addItem(newItem) - принимает новый товар newItem и добавляет его в массив товаров в свойстве items объекта который вызывает этот метод.
+removeItem(itemToRemove) - принимает товар itemToRemove и удаляет его из массива товаров в свойстве items объекта который вызывает этот метод.
+Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности, в которой твой код будут проверять тесты. 
+Пожалуйста ничего там не меняй.
 */
+
+// class Storage {
+
+//     constructor(items ) {
+//         this.items = items;
+//     }
+
+//     getItems() {
+//         return this.items;
+//     }
+
+//     addItem(newItem) {
+//         return this.items.push(newItem);
+//     }
+
+//     removeItem(itemToRemove) {
+//         return this.items.splice(this.items.indexOf(itemToRemove), 1);
+//     }
+
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 
 
